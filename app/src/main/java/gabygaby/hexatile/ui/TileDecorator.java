b53021052e2c3fe140be63410b5eeef43b6747e2
Rémi Pannequin <remi.pannequin@univ-lorraine.fr>
2015-11-02 13:54:24 +0100

    continued/fixed drawings

--- app/src/main/java/gabygaby/hexatile/ui/TileDecorator.java
+++ app/src/main/java/gabygaby/hexatile/ui/TileDecorator.java
@@ -2,6 +2,8 @@ package gabygaby.hexatile.ui;
 
 import android.graphics.Path;
 
+import gabygaby.hexatile.game.Tile;
+
 /**
  * Created by remi on 26/10/15.
  */
@@ -32,8 +34,8 @@ public class TileDecorator {
         hexa.close();
         hexa.setFillType(Path.FillType.WINDING);
 
-        vegetalLevel = new Path[6];
-        waterLevel = new Path[6];
+        vegetalLevel = new Path[Tile.MAX_TILE_LEVEL - 1];
+        waterLevel = new Path[Tile.MAX_TILE_LEVEL - 2];
 
 
         vegetalLevel[0] = new Path();
@@ -81,14 +83,20 @@ public class TileDecorator {
         vegetalLevel[5].lineTo(0, 0.5f);
         vegetalLevel[5].lineTo(-BoardView.COS/4f, 0.125f);
 
+        vegetalLevel[6] = new Path();
+
+
+
+
+
         //single circle center (0, 0.85), radius 0.5
         waterLevel[0] = new Path();
         waterLevel[0].addCircle(0, .85f, .05f, Path.Direction.CW);
 
         //previous + circle center (0, 0.65), radius 0.1
         waterLevel[1] = new Path();
-        waterLevel[0].addCircle(0, .85f, .05f, Path.Direction.CW);
-        waterLevel[0].addCircle(0, .65f, .1f, Path.Direction.CW);
+        waterLevel[1].addCircle(0, .85f, .05f, Path.Direction.CW);
+        waterLevel[1].addCircle(0, .65f, .1f, Path.Direction.CW);
 
         //vertical line from (0,0.9) to (0, 0.5)
         // + line from (-.01, 0.9) to (-0.1, 0.6) and bezier from (-0.1, 0.6) / (-0.1, 0.55)
@@ -100,44 +108,54 @@ public class TileDecorator {
         waterLevel[2].moveTo( -.1f, 0.9f);
         waterLevel[2].lineTo( -.1f, 0.6f);
         waterLevel[2].cubicTo(-.1f, 0.55f, -.15f, .5f, -.2f, .5f);
-        waterLevel[2].moveTo( 0.1f, 0.9f);
-        waterLevel[2].lineTo( 0.1f, 0.6f);
+        waterLevel[2].moveTo(0.1f, 0.9f);
+        waterLevel[2].lineTo(0.1f, 0.6f);
         waterLevel[2].cubicTo(0.1f, 0.55f, 0.15f, .5f, 0.2f, .5f);
 
         //empty "lake"
         waterLevel[3] = new Path();
-        waterLevel[3].moveTo( 0, 0.9f);
+        waterLevel[3].moveTo(0, 0.9f);
         waterLevel[3].lineTo( 0, 0.5f);
-        waterLevel[3].moveTo( 0, 0.9f);
+        waterLevel[3].moveTo(0, 0.9f);
         waterLevel[3].cubicTo(0, 0.9f, 0, .55f, -.35f, .4f);
-        waterLevel[3].moveTo( 0, 0.9f);
+        waterLevel[3].moveTo(0, 0.9f);
         waterLevel[3].cubicTo(0, 0.9f, 0, .55f, 0.35f, .4f);
+
 
         //calm lake
         waterLevel[4] = new Path();
-        waterLevel[4].moveTo( 0, 0.9f);
+        waterLevel[4].moveTo(0, 0.9f);
         waterLevel[4].lineTo( 0, 0.5f);
-        waterLevel[4].moveTo( 0, 0.9f);
+        waterLevel[4].moveTo(0, 0.9f);
         waterLevel[4].cubicTo(0, 0.9f, 0, .55f, -.35f, .4f);
-        waterLevel[4].moveTo( 0, 0.9f);
+        waterLevel[4].moveTo(0, 0.9f);
         waterLevel[4].cubicTo(0, 0.9f, 0, .55f, 0.35f, .4f);
-        waterLevel[4].moveTo( 0, 0.8f);
-        waterLevel[4].lineTo( 0, -.45f);
+        waterLevel[4].moveTo(0, 0.8f);
+        waterLevel[4].lineTo(-.35f, 0.6f);
+        waterLevel[4].moveTo(0, 0.8f);
+        waterLevel[4].lineTo( 0.35f, 0.6f);
+
 
         //calm sea
         waterLevel[5] = new Path();
-        waterLevel[5].moveTo( 0, 0.9f);
-        waterLevel[5].lineTo( 0, 0.5f);
-        waterLevel[5].moveTo( 0, 0.9f);
+        waterLevel[5].moveTo(0, 0.9f);
+        waterLevel[5].lineTo(0, 0.5f);
+        waterLevel[5].moveTo(0, 0.9f);
         waterLevel[5].cubicTo(0, 0.9f, 0, .55f, -.35f, .4f);
-        waterLevel[5].moveTo( 0, 0.9f);
+        waterLevel[5].moveTo(0, 0.9f);
         waterLevel[5].cubicTo(0, 0.9f, 0, .55f, 0.35f, .4f);
-        waterLevel[5].moveTo( 0, 0.9f);
-        waterLevel[5].cubicTo(0, 0.9f, -.02f, 0.82f, -.05f, 0.8f);
+        waterLevel[5].moveTo(    0f, 0.9f);
+        waterLevel[5].cubicTo(0f, 0.9f, -.02f, 0.82f, -.05f, 0.8f);
         waterLevel[5].cubicTo(-.08f, 0.78f, -.12f, 0.82f, -.15f, 0.8f);
         waterLevel[5].cubicTo(-.18f, 0.78f, -.17f, 0.72f, -.2f, 0.7f);
         waterLevel[5].cubicTo(-.23f, 0.68f, -.23f, 0.72f, -.3f, 0.7f);
         waterLevel[5].cubicTo(-.33f, 0.68f, -.32f, 0.62f, -.35f, 0.6f);
+        waterLevel[5].moveTo(0f, 0.9f);
+        waterLevel[5].cubicTo(0f, 0.9f, 0.02f, 0.82f, 0.05f, 0.8f);
+        waterLevel[5].cubicTo(0.08f, 0.78f, 0.12f, 0.82f, 0.15f, 0.8f);
+        waterLevel[5].cubicTo(0.18f, 0.78f, 0.17f, 0.72f, 0.2f, 0.7f);
+        waterLevel[5].cubicTo(0.23f, 0.68f, 0.23f, 0.72f, 0.3f, 0.7f);
+        waterLevel[5].cubicTo(0.33f, 0.68f, 0.32f, 0.62f, 0.35f, 0.6f);
 
         plus = new Path();
         plus.moveTo(0, 0.3f);
@@ -154,14 +172,14 @@ public class TileDecorator {
     }
 
     public Path getLevelDecoration(int level, int kind) {
-        if (level < 2) {
+        if (level < (kind + 1)) {
             return empty;
         }
         switch (kind) {
             case 1:
-                return vegetalLevel[level - 2];
+                return vegetalLevel[level - kind - 1];
             case 2:
-                return waterLevel[level - 2];
+                return waterLevel[level - kind - 1];
             case 3:
 
             case 4:
